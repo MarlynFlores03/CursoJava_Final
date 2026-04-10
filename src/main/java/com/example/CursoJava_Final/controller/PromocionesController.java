1b99c63ecc454a0d6936d510013fc1abaea41afc
Marlyn03 <marlynflores93@gmail.com>
2026-04-10 12:44:15 +0200

    Se actualizó Controller promociones

--- src/main/java/com/example/CursoJava_Final/controller/PromocionesController.java
+++ src/main/java/com/example/CursoJava_Final/controller/PromocionesController.java
@@ -1,15 +1,42 @@
 package com.example.CursoJava_Final.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.CursoJava_Final.model.Promociones;
+import com.example.CursoJava_Final.PromocionesService;
 
 @RestController
 @RequestMapping("/api/promociones")
 public class PromocionesController {
+    @Autowired
+    private PromocionesService promocionesService;
 
     @GetMapping
-    public String getPromociones() {
-        return "Promociones";
+    public List<Promociones> getAllProducts() {
+        return promocionesService.getAllPromociones();
+    }
+
+    @GetMapping("/{id}")
+    public Promociones getProductById(@PathVariable Long id) {
+        return promocionesService.getPromocionesById(id);
+    }
+
+    @PostMapping
+    public Promociones saveProduct(@RequestBody Promociones promociones) {
+        return promocionesService.savePromociones(promociones);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        promocionesService.deletePromociones(id);
     }
 }
